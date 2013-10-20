9c693f35a7da83faee310e34537f9c25439ee751
kushalarora <kushal18@gmail.com>
2013-10-20 14:21:47 -0400

    some graph changes

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -1,100 +1,21 @@
-/*
- * GRAPH ALOGRITHMS
- *
- * 1. Types of Graphs
- *      Undirected vs Directed
- *      Weighted vs Unweighted
- *          The difference is particularly apperant in shortest path problem.
- *          For Unweighted graph its BFS. For Weighted Graph we need Djzxtra and Bellman Ford.
- *      Simple vs Non Simple
- *          Self Loop and multiedges present - Non Simple
- *      Sparse vs Dense
- *          Typically is edges are linear to vertices, its sparse.
- *          If quadratic its dense.
- *      Cyclic vs Acyclic
- *          Acyclic doesn't contain any cycles.
- *          Example Trees(Connected Acyclic Undirected Graphs{Hoping you use doubly linked structure})
- *          Directed Acyclic Graph, example scheduling problem.
- *          Topological Sorting orders the vertices of *DAG* according to constraints.
- *          Topological Sorting is first step in any DAG problem.
- *      Embedded vs Topological
- *          Graph embedded in space where each vertices is assigned a geometric position.
- *          Topological graph are where geometry of the graph is important.
- *
- * 2. Representation of a Graph
- *      1. Adjacency Matrix
- *          M[i, j] =   1 if (i,j) has an edge else 0
- *          Faster queries like "is (i,j) in G, faster updates, deletion
- *          Excessive space for sparse graph. Used only for dense graph.
- *
- *      2. Adjacency List
- *          Used all the time as most real life graphs are sparse.
- *          Linked representation of graph.
- *          An array of nodes containing all vertices.
- *          Each array node is a linked list containing all edges directed outward/inwards of the graph.
- *          Generally outward is used.
- *          Each node contains
- *              i.   Label
- *              ii.  Weight
- *              iii. Value
- *              iv.  Pointer to next edge node.
- *
- *          Apart from this the main structure contains some journaling info like
- *              i.   Array of degree of each node.
- *              ii.  No. of vertices.
- *              iii. No. of edges
- *              iv.  boolean directed.
- */
+#include "graph.hpp"
 
-#include<iostream>
-#include<string>
-#include<stdlib.h>     // for rand()
-#include<time.h>
-#include<assert.h>
-
-using namespace std;
-const int MAXV = 100000;
-static bool DEBUG;
-
-// forward declaration to resolve circular dependency.
-class Edge;
-
-class Node {
-    string label;   // labels are unique
-    int value;      // need not be unique
-    int weight;
-    Edge* edge_list;     // linked list of edges.
-    bool operator ==(Node* node2);
-    int adj_index;
-
-    public:
-        Node() {}
-        Node(int value, int weight, string label);
-        Node(Node* node);
-        int getValue() {return value;}
-        void setValue(int val) {value = val;}
-        Edge* getEdgeList() {return edge_list;}
-        Edge* setEdgeList(Edge* edge) {edge_list = edge;}
-        int getWeight() {return weight;}
-        void setWeight(int wght) {weight = wght;}
-        string getLabel() {return label;}
-        void setLabel(string lbl) {label = lbl;}
-        void setAdjecencyIndex(int val) {adj_index = val;}
-        int  getAdjecencyIndex() {return adj_index;}
-
-};
-
-Node::Node(int val, int wght, string lbl) {
+Node::Node(int val, string lbl) {
     value = val;
-    weight = wght;
     label = lbl;
     edge_list = NULL;
     adj_index = -1;
 }
 
+Node::Node(int val) {
+    Node(val, NULL);
+}
+Node::Node(string lbl) {
+    Node(-1, lbl);
+}
+
 Node::Node(Node* node) {
     value = node->getValue();
-    weight = node->getWeight();
     label = node->getLabel();
     edge_list = NULL;
     adj_index = node->getAdjecencyIndex();
@@ -108,28 +29,6 @@ bool Node::operator==(Node* node2) {
         return this->label == node2->label;
     return false;
 };
-
-class Edge {
-    Node* node1;
-    Node* node2;
-    bool is_directed;
-    float weight;
-    Edge* next;
-    const static float DEFAULT_WEIGHT = 0.0f;
-    public:
-        Edge(Node* n1, Node* n2);
-        Edge(Node* n1, Node* n2, bool is_directed, float weight);
-        Edge(Node* n1, Node* n2, bool is_directed);
-        Edge(Node* n1, Node* n2, float weight);
-        Edge* getNext() {return next;}
-        void setNext(Edge* next) {this->next = next;}
-        Node* getCurrentNode() {return node1;}
-        Node* getOtherNode() { return node2;}
-        bool isDirected() {return is_directed;}
-        float getWeight() {return weight;}
-        bool operator ==(Edge* edge2);
-};
-
 Edge::Edge(Node* n1, Node* n2, bool is_directed, float weight) {
     node1 = n1;
     node2 = n2;
@@ -161,43 +60,6 @@ bool Edge::operator ==(Edge* edge2) {
     return false;
 }
 
-class Graph {
-    public:
-        Graph();
-        ~Graph();
-        Graph(bool directed, bool weighted, bool labelled, bool valued);
-        Node* insertNode(Node* node);
-        Node* insertNode(int value);
-        Node* insertNode(int value, int weight);
-        Node* insertNode(int value, string label);
-        Node* insertNode(string label, int weight);
-        Node* insertNode(string label);
-        Node* insertNode(int value, int weight, string label);
-        void createEdge(Node* V1, Node* V2);
-        void createEdge(Node* V1, Node* V2, float weight);
-        void createEdge(string label1, string label2);
-        void createEdge(string label1, string label2, float weight);
-        inline bool isWeighted() {return weighted;}
-        inline bool isDirected() {return directed;}
-        inline bool isLabelled() {return labelled;}
-        inline bool isValued() {return valued;}
-        Node* searchNodeByLabel(string label);
-        void printGraph();
-        void createRandomGraph(int nVertices);
-        void createRandomGraph(int nVertices, float density);
-    private:
-        int nVertices;
-        int nEdges;
-        bool directed;
-        bool weighted;
-        bool labelled;
-        bool valued;
-        int degree[MAXV];
-        Node *edgeNode[MAXV];
-        void printNode(Node* node);
-        void printEdge(Edge* edge);
-        string createRandomLabels(int nVertices);
-};
 
 Graph::Graph(bool dirctd, bool wghtd, bool lbled, bool valed) {
     nVertices = 0;
@@ -231,32 +93,17 @@ Node* Graph::insertNode(Node* node) {
 }
 Node* Graph::insertNode(int value) {
     assert(isValued());
-    return insertNode(new Node(value, 0, NULL));
-}
-
-Node* Graph::insertNode(int value, int weight) {
-    assert(isValued() && isWeighted());
-    return insertNode(new Node(value, weight, NULL));
-}
-
-Node* Graph::insertNode(int value, int weight, string label) {
-    assert(isValued() && isWeighted() && isLabelled());
-    return insertNode(new Node(value, weight, label));
+    return insertNode(new Node(value, NULL));
 }
 
 Node* Graph::insertNode(int value, string label) {
     assert(isValued() && isLabelled());
-    return insertNode(new Node(value, 0, label));
-}
-
-Node* Graph::insertNode(string label, int weight) {
-    assert(isLabelled() && isValued());
-    return insertNode(new Node(0, weight, label));
+    return insertNode(new Node(value, label));
 }
 
 Node* Graph::insertNode(string label) {
     assert(isLabelled());
-    return insertNode(new Node(0, 0, label));
+    return insertNode(new Node(0, label));
 }
 
 Node* Graph::searchNodeByLabel(string label) {
@@ -322,40 +169,36 @@ void Graph::createEdge(string label1, string label2) {
 void Graph::printGraph() {
     for (int i = 0; i < nVertices; i++) {
         Node* node = edgeNode[i];
-        printNode(node);
+        node->printNode();
         Edge* tmp = node->getEdgeList();
         while (tmp != NULL) {
             assert(tmp->getCurrentNode() == node);
-            printEdge(tmp);
-            printNode(tmp->getOtherNode());
+            tmp->printEdge();
+            (tmp->getOtherNode())->printNode();
             tmp = tmp->getNext();
         }
         cout<<"\n"<<"\n";
     }
 }
 
-void Graph::printNode(Node* node) {
-    if (node == NULL)
-        return;
+void Node::printNode() {
     cout << "( ";
-    if (isLabelled() && node->getLabel().length() > 0)
-        cout << node->getLabel() << " ";
-     if (isValued() && node->getValue() > 0)
-         cout << node->getValue() << " ";
+    if (this->getLabel().length() > 0)
+        cout << getLabel() << " ";
+     if (getValue() > 0)
+         cout << getValue() << " ";
      cout << ")";
 }
 
-void Graph::printEdge(Edge* edge) {
-    if (edge == NULL)
-        return;
+void Edge::printEdge() {
     cout << (isDirected() ? "--" : "<--");
-    if (isWeighted())
-        cout << edge->getWeight();
+    if (getWeight() != DEFAULT_WEIGHT)
+        cout << getWeight();
     cout << "-->";
 }
 
 
-string Graph::createRandomLabels(int nVertices) {
+string Node::createRandomLabels(int nVertices) {
     int len = nVertices < 10 ? 2 : nVertices < 100 ? 4 : nVertices < 1000 ? 5 : 8;
     char sbuilder[len];
     for (int i = 0; i < len; i++) {
@@ -365,18 +208,22 @@ string Graph::createRandomLabels(int nVertices) {
     return str;
 }
 
+Node* Node::populateNode(bool labelled, bool valued, int seed) {
+    if (labelled)
+        this->setLabel(createRandomLabels(seed));
+
+    if (valued)
+        this->setValue(rand() % seed + 1);
+}
+
 void Graph::createRandomGraph(int nVertices, float density) {
     srand(time(NULL));
     if (nVertices < 1)
         return;
     for (int i = 0; i < nVertices; i++) {
-        Node* node = new Node();
-        if (isLabelled())
-            node->setLabel(createRandomLabels(nVertices));
-
-        if (isValued())
-            node->setValue(rand() % nVertices + 1);
-        insertNode(node);
+        insertNode((new Node())->populateNode(isLabelled(),
+                                    isValued(),
+                                    nVertices));
     }
     int nEdges = nVertices * (density > 0.0 ? density * nVertices : (rand() % nVertices));
     cout<<"\nMaking "<<nEdges<<" Edges\n";
@@ -397,27 +244,17 @@ void Graph::createRandomGraph(int nVertices) {
     createRandomGraph(nVertices, 0.0);
 }
 
+static int count = 0;
 Graph::~Graph() {
     for (int i = 0; i < nVertices; i++) {
         Node* tmp = edgeNode[i];
         Edge* tmp2 = tmp->getEdgeList();
-
-        delete tmp;
         Edge *tmp3;
         while(tmp2 != NULL) {
             tmp3 = tmp2->getNext();
             delete tmp2;
             tmp2 = tmp3;
         }
+        delete tmp;
     }
-}
-
-int main(int argc, char *argv[]) {
-    if (argc > 1)
-        return -1;
-    DEBUG = argc == 1 && argv[0][0] - '1' > 0;
-    Graph g = Graph(false, true, true, true);
-    g.createRandomGraph(10);
-    g.printGraph();
-    return 0;
 }
---
+++ graph/graph.hpp
@@ -0,0 +1,146 @@
+/*
+ * GRAPH ALOGRITHMS
+ *
+ * 1. Types of Graphs
+ *      Undirected vs Directed
+ *      Weighted vs Unweighted
+ *          The difference is particularly apperant in shortest path problem.
+ *          For Unweighted graph its BFS. For Weighted Graph we need Djzxtra and Bellman Ford.
+ *      Simple vs Non Simple
+ *          Self Loop and multiedges present - Non Simple
+ *      Sparse vs Dense
+ *          Typically is edges are linear to vertices, its sparse.
+ *          If quadratic its dense.
+ *      Cyclic vs Acyclic
+ *          Acyclic doesn't contain any cycles.
+ *          Example Trees(Connected Acyclic Undirected Graphs{Hoping you use doubly linked structure})
+ *          Directed Acyclic Graph, example scheduling problem.
+ *          Topological Sorting orders the vertices of *DAG* according to constraints.
+ *          Topological Sorting is first step in any DAG problem.
+ *      Embedded vs Topological
+ *          Graph embedded in space where each vertices is assigned a geometric position.
+ *          Topological graph are where geometry of the graph is important.
+ *
+ * 2. Representation of a Graph
+ *      1. Adjacency Matrix
+ *          M[i, j] =   1 if (i,j) has an edge else 0
+ *          Faster queries like "is (i,j) in G, faster updates, deletion
+ *          Excessive space for sparse graph. Used only for dense graph.
+ *
+ *      2. Adjacency List
+ *          Used all the time as most real life graphs are sparse.
+ *          Linked representation of graph.
+ *          An array of nodes containing all vertices.
+ *          Each array node is a linked list containing all edges directed outward/inwards of the graph.
+ *          Generally outward is used.
+ *          Each node contains
+ *              i.   Label
+ *              ii.  Weight
+ *              iii. Value
+ *              iv.  Pointer to next edge node.
+ *
+ *          Apart from this the main structure contains some journaling info like
+ *              i.   Array of degree of each node.
+ *              ii.  No. of vertices.
+ *              iii. No. of edges
+ *              iv.  boolean directed.
+ */
+
+#include<iostream>
+#include<string>
+#include<stdlib.h>     // for rand()
+#include<time.h>
+#include<assert.h>
+
+using namespace std;
+
+#ifndef GRAPH
+#define GRAPH
+const int MAXV = 100000;
+
+// forward declaration to resolve circular dependency.
+class Edge;
+
+class Node {
+    string label;   // labels are unique
+    int value;      // need not be unique
+    Edge* edge_list;     // linked list of edges.
+    bool operator ==(Node* node2);
+    int adj_index;
+    string createRandomLabels(int nVertices);
+
+    public:
+        Node() {}
+        Node(int value);
+        Node(string label);
+        Node(int value, string label);
+        Node(Node* node);
+        int getValue() {return value;}
+        void setValue(int val) {value = val;}
+        Edge* getEdgeList() {return edge_list;}
+        Edge* setEdgeList(Edge* edge) {edge_list = edge;}
+        string getLabel() {return label;}
+        void setLabel(string lbl) {label = lbl;}
+        void setAdjecencyIndex(int val) {adj_index = val;}
+        int  getAdjecencyIndex() {return adj_index;}
+        virtual Node* populateNode(bool labelled, bool valued, int seed);
+        void printNode();
+
+};
+
+
+class Edge {
+    Node* node1;
+    Node* node2;
+    bool is_directed;
+    float weight;
+    Edge* next;
+    const static float DEFAULT_WEIGHT = 0.0f;
+    public:
+        Edge(Node* n1, Node* n2);
+        Edge(Node* n1, Node* n2, bool is_directed, float weight);
+        Edge(Node* n1, Node* n2, bool is_directed);
+        Edge(Node* n1, Node* n2, float weight);
+        Edge* getNext() {return next;}
+        void setNext(Edge* next) {this->next = next;}
+        Node* getCurrentNode() {return node1;}
+        Node* getOtherNode() { return node2;}
+        bool isDirected() {return is_directed;}
+        float getWeight() {return weight;}
+        bool operator ==(Edge* edge2);
+        void printEdge();
+};
+
+class Graph {
+    public:
+        Graph();
+        ~Graph();
+        Graph(bool directed, bool weighted, bool labelled, bool valued);
+        Node* insertNode(Node* node);
+        Node* insertNode(int value);
+        Node* insertNode(string label);
+        Node* insertNode(int value, string label);
+        void createEdge(Node* V1, Node* V2);
+        void createEdge(Node* V1, Node* V2, float weight);
+        void createEdge(string label1, string label2);
+        void createEdge(string label1, string label2, float weight);
+        inline bool isWeighted() {return weighted;}
+        inline bool isDirected() {return directed;}
+        inline bool isLabelled() {return labelled;}
+        inline bool isValued() {return valued;}
+        Node* searchNodeByLabel(string label);
+        void printGraph();
+        void createRandomGraph(int nVertices);
+        void createRandomGraph(int nVertices, float density);
+    private:
+        int nVertices;
+        int nEdges;
+        bool directed;
+        bool weighted;
+        bool labelled;
+        bool valued;
+        int degree[MAXV];
+        Node *edgeNode[MAXV];
+};
+
+#endif
